d95e43c8d03de97261a74675af2758df36db251f
VectorVP <56307628+VectorVP@users.noreply.github.com>
2020-07-27 14:27:50 +0300

    Update disc.py
    Train update

--- disc.py
+++ disc.py
@@ -33,7 +33,9 @@ import json
 import datetime
 import numpy as np
 import skimage.draw
+import glob
 
+from pathlib import Path
 import warnings
 warnings.filterwarnings("ignore")
 import tensorflow as tf
@@ -66,17 +68,17 @@ class DiscConfig(Config):
     Derives from the base Config class and overrides some values.
     """
     # Give the configuration a recognizable name
-    NAME = "disc"
+    NAME = "object"
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
     IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # Background + disc
+    NUM_CLASSES = 1 + 4  # Background + disc
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 100
+    STEPS_PER_EPOCH = 200
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
@@ -94,59 +96,50 @@ class DiscDataset(utils.Dataset):
         subset: Subset to load: train or val
         """
         # Add classes. We have only one class to add.
-        self.add_class("disc", 1, "disc")
+        self.add_class("object", 1, "disc")
+        self.add_class("object", 2, "spinal canal")
+        self.add_class("object", 3, "nerv")
+        self.add_class("object", 4, "neural foramen")
 
         # Train or validation dataset?
-        assert subset in ["train", "val"]
+        assert subset in ["train", "val", "test"]
         dataset_dir = os.path.join(dataset_dir, subset)
+        
+        for subdir, dirs, files in os.walk(dataset_dir):    
+            for i in dirs:
+                current_dir = os.path.join(subdir, i)
+                
+                for file_name in os.listdir(current_dir):
+                    if file_name.endswith(".json"):
+                        jsn = os.path.join(current_dir, file_name)
+                annotations = json.load(open(jsn))
+                annotations = list(annotations['_via_img_metadata'].values())
+                annotations = [a for a in annotations if a['regions']]
 
-        # Load annotations
-        # VGG Image Annotator (up to version 1.6) saves each image in the form:
-        # { 'filename': '28503151_5b5b7ec140_b.jpg',
-        #   'regions': {
-        #       '0': {
-        #           'region_attributes': {},
-        #           'shape_attributes': {
-        #               'all_points_x': [...],
-        #               'all_points_y': [...],
-        #               'name': 'polygon'}},
-        #       ... more regions ...
-        #   },
-        #   'size': 100202
-        # }
-        # We mostly care about the x and y coordinates of each region
-        # Note: In VIA 2.0, regions was changed from a dict to a list.
-        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
-        #annotations = list(annotations.values())  # don't need the dict keys
-        annotations = list(annotations['_via_img_metadata'].values())
-        # The VIA tool saves images in the JSON even if they don't have any
-        # annotations. Skip unannotated images.
-        annotations = [a for a in annotations if a['regions']]
+                # Add images
+                for a in annotations:
+                    polygons = [r['shape_attributes'] for r in a['regions'] if r['shape_attributes']['name'] != "rect"] 
+            
+                    if not polygons:
+                        continue
+            
+                    objects = [s["region_attributes"]["А3"] for s in a["regions"]]
+                    if 'defective' in objects:
+                        continue
+            
+                    name_dict = {"disc": 1,"spinal canal": 2, "nerv": 3, "neural foramen ": 4}
+                    num_ids = [name_dict[a] for a in objects if a!='defective']
+                    image_path = os.path.join(current_dir, a['filename'])
+                    image = skimage.io.imread(image_path)
+                    height, width = image.shape[:2]
 
-        # Add images
-        for a in annotations:
-            # Get the x, y coordinaets of points of the polygons that make up
-            # the outline of each object instance. These are stores in the
-            # shape_attributes (see json format above)
-            # The if condition is needed to support VIA versions 1.x and 2.x.
-            #if type(a['regions']) is dict:
-            #    polygons = [r['shape_attributes'] for r in a['regions'].values()]
-            #else:
-            polygons = [r['shape_attributes'] for r in a['regions']] 
-
-            # load_mask() needs the image size to convert polygons to masks.
-            # Unfortunately, VIA doesn't include it in JSON, so we must read
-            # the image. This is only managable since the dataset is tiny.
-            image_path = os.path.join(dataset_dir, a['filename'])
-            image = skimage.io.imread(image_path)
-            height, width = image.shape[:2]
-
-            self.add_image(
-                "disc",
-                image_id=a['filename'],  # use file name as a unique image id
-                path=image_path,
-                width=width, height=height,
-                polygons=polygons)
+                    self.add_image(
+                        "object",
+                        image_id=f"{current_dir}_{a['filename']}",  # use file name as a unique image id
+                        path=image_path,
+                        width=width, height=height,
+                        polygons=polygons,
+                        num_ids=num_ids)
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -157,9 +150,9 @@ class DiscDataset(utils.Dataset):
         """
         # If not a disc dataset image, delegate to parent class.
         image_info = self.image_info[image_id]
-        if image_info["source"] != "disc":
+        if image_info["source"] != "object":
             return super(self.__class__, self).load_mask(image_id)
-
+        
         # Convert polygons to a bitmap mask of shape
         # [height, width, instance_count]
         info = self.image_info[image_id]
@@ -169,15 +162,17 @@ class DiscDataset(utils.Dataset):
             # Get indexes of pixels inside the polygon and set them to 1
             rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
             mask[rr, cc, i] = 1
-
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
-        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
+        #return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
+    
+        num_ids = np.array(info['num_ids'], dtype=np.int32)
+        return mask, num_ids
 
     def image_reference(self, image_id):
         """Return the path of the image."""
         info = self.image_info[image_id]
-        if info["source"] == "disc":
+        if info["source"] == "object":
             return info["path"]
         else:
             super(self.__class__, self).image_reference(image_id)
@@ -202,7 +197,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=1,
+                epochs=1000,
                 layers='heads')
 
 
